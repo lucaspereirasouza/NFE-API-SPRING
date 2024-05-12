cadb0abb149b84e217752c9a6064a3483e40f2a6
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2024-05-11 23:41:49 -0300

    feat(HttpMessage): Configured default BufferedImage Response

--- src/main/java/com/inovacoes/exame/ExamApplication.java
+++ src/main/java/com/inovacoes/exame/ExamApplication.java
@@ -1,14 +1,16 @@
 package com.inovacoes.exame;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.image.BufferedImage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,13 +20,24 @@ public class ExamApplication {
 		SpringApplication.run(ExamApplication.class, args);
 	}
 
-    //bean later
+    @Bean
+    HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+	    return new BufferedImageHttpMessageConverter();
+	}
 	
 	@Configuration
-	public class WebConfig implements WebMvcConfigurer {
+	class WebConfig implements WebMvcConfigurer {
 	  public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
 	    configurer.defaultContentType(MediaType.APPLICATION_XML);
 	  }
 	}
-
+	
+//	@Configuration
+//	@EnableMethodSecurity(
+//	  prePostEnabled = true, 
+//	  securedEnabled = true, 
+//	  jsr250Enabled = true)
+//	public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration{
+//		
+//	}
 }
