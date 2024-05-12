90eb6c324e345f845b6ed8f6482f1cf317c94e46
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2024-05-11 23:40:37 -0300

    feat: add NFe script W.I.P

--- front/js/submit.js
+++ front/js/submit.js
@@ -1,39 +1,38 @@
-const fileInput = document.querySelector("input[type=file]");
+var fileInput = document.querySelector("input[type='file']");
 const output = document.querySelector(".output");
-const submit = document.querySelector("form")
+const submit_query = document.querySelector("button[type=submit]");
+const url = "http://127.0.0.1:8080/v1/inovacoes/nfe/post";
+var [file] = fileInput.files;
 
-const url = "http://127.0.0.1:8080/v1/inovacoes/xml/post"
-const [file] = fileInput.files;
+async function onclickevent(){
+    
+    console.log("funciona");
+    console.log();
+    
+    [file] = fileInput.files;
+    xmlValue = await file.text();
 
-const parser = new DOMParser();
-const _parser = new XMLSerializer;
-// const xmld = XMLDocument;
-
-const stream = parser.parseFromString(file.text(),"application/xml");
-
-
-
-submit.addEventListener("submit",async () =>{
-    const response = await fetch(url,
+    console.log(file.text());
+    const dom = new DOMParser().parseFromString(file.value,"application/xml");
+    const response = await fetch(
+        url,
         {
-        method:"POST",
+        method:"post",
         mode: "no-cors",
-        cache:"no-cache",
-        body:file,
+        body:dom,
         headers:{
-
-        
-            "Accept": 'application/xml',
-            "Content-Type": 'application/xml',
-            "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/xml",
+            "ACCEPT": "application/xml",
+            "Access-Control-Allow-Origin": "*",
         }
         }
     )
+    .then((rs)=>{
+        if (file) {
+            output.innerText = dom;
+          }
+    })
     .catch((err)=>console.log(err))
-    if (file) {
-    
-      output.innerText = await file.text();
-    }
+}
 
-});
+
